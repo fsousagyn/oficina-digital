c153267bc62deff34c4fee64e2330e4f7cc2ea60
fsousagyn <fsousa.gyn@gmail.com>
2025-07-16 20:39:52 -0300

    atualizacao quarta160725

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,13 +3,21 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>© 2025 EF Criativa · MarcenarIA & SerralherIA</p>
-        <p>Contato: efcriativa@email.com | (62) 99620-4767</p>
-        <div className="redes">
-          <a href="https://instagram.com/efcriativa" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="https://wa.me/5562996204767" target="_blank" rel="noopener noreferrer">WhatsApp</a>
-        </div>
+      <p>© 2025 EF Criativa. Todos os direitos reservados.</p>
+      <p>Contato: contato@efcriativa.com.br | (62) 99620-4767</p>
+      <div className="social-icons">
+        <a href="https://www.instagram.com/efcriativa" target="_blank" rel="noopener noreferrer">
+          <img src="/imagens/instagram.png" alt="Instagram" />
+        </a>
+		    <a href="https://www.facebook.com/efcriativa" target="_blank" rel="noopener noreferrer">
+          <img src="/imagens/facebook.png" alt="Facebook" />
+        </a>
+		    <a href="https://www.youtube.com/madeirametalecerveja" target="_blank" rel="noopener noreferrer">
+          <img src="/imagens/youtube.png" alt="YouTube" />
+          </a>
+        <a href="https://wa.me/5562996204767" target="_blank" rel="noopener noreferrer">
+          <img src="/imagens/whatsapp.png" alt="WhatsApp" />
+        </a>
       </div>
     </footer>
   );
